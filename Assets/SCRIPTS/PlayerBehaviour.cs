487685456e87928e9ac1be640d4760b57d13bdb9
SaraPerezMonroy <saraiperezmonroy@gmail.com>
2024-02-22 10:46:28 +0000

    movimiento

--- Assets/SCRIPTS/PlayerBehaviour.cs
+++ Assets/SCRIPTS/PlayerBehaviour.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    Rigidbody rb;
+    public float playerSpeed;
+
+    public Vector2 turn;
+    public float sensitivity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); // Acceder al rigidbody
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.position += transform.forward * playerSpeed * Time.deltaTime;
+        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
     }
 }
