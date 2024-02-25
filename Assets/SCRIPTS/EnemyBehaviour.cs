45d001926c67e8f4ae8efaa3080f57ffbbc367f2
SaraPerezMonroy <saraiperezmonroy@gmail.com>
2024-02-25 23:04:01 +0000

    Cinemática

--- Assets/SCRIPTS/EnemyBehaviour.cs
+++ Assets/SCRIPTS/EnemyBehaviour.cs
@@ -8,6 +8,7 @@ public class EnemyBehaviour : MonoBehaviour
     public Transform[] waypoints; // Array con los puntos a los que ir
     int currentWaypointIndex;
     public float enemySpeed = 20f;
+    public float rotationSpeed = 1f;
 
     [SerializeField]
     public ParticleSystem explosion;
@@ -27,8 +28,9 @@ public class EnemyBehaviour : MonoBehaviour
         Transform waypointTarget = waypoints[currentWaypointIndex];
         Vector3 waypointDirection = (waypointTarget.position - transform.position).normalized;
         transform.position += waypointDirection * enemySpeed * Time.deltaTime;
-        transform.LookAt(waypointTarget); // Para hacer que mire al waypoint al que tiene que ir
-        
+        Quaternion targetRotation = Quaternion.LookRotation(waypointDirection); // Para que gire más suave
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); 
+
         if (Vector3.Distance(transform.position, waypointTarget.position) < 0.5f) // Si la nave llega al waypoint actual, pasa al siguiente waypoint. Distance pide 2 valores, tenemos posición actual y posición del siguiente waypoint
         {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
